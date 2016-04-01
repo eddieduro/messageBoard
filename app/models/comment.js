376907ee51dfe2e0b7359c14034db905f1be73aa
eddieduro <e.jamesduro@gmail.com>
2016-04-01 08:28:15 -0700

    update json to show comments

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  message: DS.attr(),
+  author: DS.attr(),
+  date: DS.attr(),
+  post: DS.belongsTo('post', {async: true})
 });
