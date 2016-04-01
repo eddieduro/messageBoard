d58fd6a09dc65924ed1bcb7f15a3e6ae8f195949
eddieduro <e.jamesduro@gmail.com>
2016-04-01 09:03:08 -0700

    add functionality for saving and displaying new questions

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showNewPostForm: false,
+  actions: {
+    showForm(){
+      this.set('showNewPostForm', true);
+    },
+    savePost(){
+      var params= {
+        title: this.get('title'),
+        message: this.get('message'),
+        author: this.get('author'),
+        date: this.get('date'),
+        comments: this.get('comments'),
+      };
+      this.set('showNewPostForm', false);
+      this.sendAction("savePost", params)
+    }
+  }
 });
