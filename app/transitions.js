507e1105092794a27c47be2071c863681d01920d
eddieduro <e.jamesduro@gmail.com>
2016-04-01 15:14:47 -0700

    add animation to all form reveals

--- app/transitions.js
+++ app/transitions.js
@@ -11,10 +11,10 @@ export default function(){
    // this makes our rule apply when the liquid-if transitions to the
    // true state.
    this.toValue(true),
-   this.use('crossFade'),
+   this.use('toDown', {duration: 300, easing: 'ease'}),
 
    // which means we can also apply a reverse rule for transitions to
    // the false state.
-   this.reverse('crossFade')
+   this.reverse('toUp', {duration: 300, easing: 'ease'})
  );
 }
